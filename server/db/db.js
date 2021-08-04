35a3ffe715f9cdd926aa5bc0d91a20a52bfa6e23
unknown <itamar.hagai1@gmail.com>
2021-08-04 09:10:44 +0300

    home little advanced

--- server/db/db.js
+++ server/db/db.js
@@ -21,35 +21,4 @@ connection.on('connect', function(err) {
 
 connection.connect();
 
-
-// const Request = require('tedious').Request;  
-// const TYPES = require('tedious').TYPES;  
-
-// function executeStatement() {  
-//     request = new Request("SELECT * from tblUsers", function(err) {  
-//     if (err) {  
-//         console.log(err);}  
-//     });  
-//     const result = "";  
-//     request.on('row', function(columns) {  
-//         columns.forEach(function(column) {  
-//           if (column.value === null) {  
-//             console.log('NULL');  
-//           } else {  
-//             result+= column.value + " ";  
-//           }  
-//         });  
-//         console.log(result);  
-//         result ="";  
-//     });  
-
-//     request.on('done', function(rowCount, more) {  
-//     console.log(rowCount + ' rows returned');  
-//     });  
-    
-//     // Close the connection after the final event emitted by the request, after the callback passes
-//     request.on("requestCompleted", function (rowCount, more) {
-//         connection.close();
-//     });
-//     connection.execSql(request);  
-// }  
+module.exports=connection;
